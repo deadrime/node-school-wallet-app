2f83401f268ceb162ff46efb80c5aae24c0727e3
zhenya <deadrime@mail.ru>
2017-10-13 01:46:06 +0300

    Добавил контроллеры, расширенную ошибку, модульность, много модульности.

--- index.js
+++ index.js
@@ -7,40 +7,10 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-let bd = JSON.parse(fs.readFileSync('./source/cards.json', 'utf8'));
 
-function isValid(card) {
-	if ('balance' in card) {
-		let cardBalance = card.balance;
-		let validBalance = Number.isInteger(cardBalance); // вот тут не уверен.
-		if (!validBalance) return false
-	}
-	else {
-		card['balance'] = 0 // если поля нет - можно его добавить.
-	}
-	if ('cardNumber' in card) {
-		let cardNumber = card.cardNumber;
-		let length = cardNumber.length;
-		let validLength = length === 16;
-		let validNumber = /^[0-9]*$/.test(cardNumber);
-		let alreadyInBd = bd.some(c => c.cardNumber === cardNumber);
-		if (validLength && validNumber && !alreadyInBd) {
-			return true
-		}
-	}
-	return false
-}
-
-function findCard(cardNumber) {
-	for (let [id,card] of bd.entries()) {
-		//console.log(card);
-		//console.log(card.cardNumber);
-		if (card.cardNumber === cardNumber) {
-			return id;
-		}
-	}
-	return false;
-}
+const getCardsController = require('./controllers/get-cards');
+const addCardController = require('./controllers/add-card');
+const deleteCardController = require('./controllers/delete-cards');
 
 app.get('/', (req, res) => {
 	res.send(`<!doctype html>
@@ -49,38 +19,14 @@ app.get('/', (req, res) => {
 			<link rel="stylesheet" href="/style.css">
 		</head>
 		<body>
-			<h1>Hello Smolny!</h1>
+			<h1>Hello World!</h1>
 		</body>
 	</html>`);
 });
 
-app.get('/cards', (req, res) => {
-	res.send(bd);
-});
-
-app.post('/cards', (req, res) => {
-	let newCard = req.body;
-	//console.log(newCard);
-	if (isValid(newCard)) {
-		bd.push(newCard);
-		res.send(newCard);
-	}
-	else {
-		res.status(400).send('Bad request');
-	}
-});
-
-app.delete('/cards/:cardNumber', (req, res) => {
-	let cardNumber = req.params.cardNumber.toString();
-	let id = findCard(cardNumber);
-	if (id) {
-		bd.splice(id, 1);
-		res.send('Success');
-	}
-	else {
-		res.status(404).send('Card not found');
-	}
-});
+app.get('/cards', getCardsController);
+app.post('/cards', addCardController);
+app.delete('/cards/:cardNumber', deleteCardController);
 
 app.get('/error', (req, res) => {
 	throw Error('Oops!');
